cecb0d8d6d61dd5401405d350dd410e99467e5b3
Sergey R. <s.romanovsskii@gmail.com>
2025-10-19 16:51:29 +0300

    new feature

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -3,5 +3,6 @@ package org.example;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Привет, мир, Hello Vitaly!");
+        System.out.println("Довалена новая функциональсть");
     }
 }
